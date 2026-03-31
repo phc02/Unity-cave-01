1b27dd70e793b4bc1321ef4e172df123badd93e8
phc02 <chaopingliangankuo@gmail.com>
2026-03-31 19:54:41 +0800

    移动摄像机
    在场景中添加了一个胶囊体和相机，为相机添加了CameraController脚本

---
+++ Cave-01/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform player;
+    
+    private float mouseX,mouseY;//获取鼠标移动的值
+
+    public float mouseSensitivity;//鼠标灵敏度
+
+    private void Update(){
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.Rotate(Vector3.up * mouseX);
+        trasform.localRotation = Quaternion.Euler(-mouseY,0,0)
+    }
+}
